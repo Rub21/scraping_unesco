4a2ec36917200fd807f5a0682993ec488ea91a4d
Rub21 <rub2106@gmail.com>
2013-04-30 17:17:01 -0500

    agragando cordenadas el geojson

--- scraping/web/scraping PH/app.js
+++ scraping/web/scraping PH/app.js
@@ -13,8 +13,6 @@ for (i = 1; i < 3; i++) {
 //get print data  
 
     var urlwidget = $('#widgetscript').attr('urlwidget');
-
-
     $.get(urlwidget, function(res) {
         //parametro para eliminar string
         var first = res.responseText.search('<div class="relatedContent alternate">');
@@ -45,8 +43,9 @@ for (i = 1; i < 3; i++) {
             }
         };
 
+        console.log('CONTADOR DE CHILDRENS');
         console.log($(data)[1].previousSibling.childElementCount);
-        console.log($(data)[1].previousSibling.children[0].innerText);
+
         if (data.length > 0) {
             all += data;
             if ($(data)[1].previousSibling.childElementCount === 7) {
@@ -65,44 +64,68 @@ for (i = 1; i < 3; i++) {
 
                 d.properties.property = $(data)[1].previousSibling.children[5].innerText;
                 d.properties.property = d.properties.property.replace(/\n/g, '').replace('Property :', '');
-                geojson.features.push(d);
+
 
             } else if ($(data)[1].previousSibling.childElementCount === 8) {
 
-                console.log($(data)[1]);
-                 d.properties.country = $(data)[1].previousSibling.children[0].innerText;
-                 d.properties.country = d.properties.country.replace(/\n/g, '');
-                 
-                 d.properties.name = $(data)[1].previousSibling.children[1].innerText;
-                 d.properties.name = d.properties.name.replace(/\n/g, '');
-                 
-                 d.properties.dateofinscription = $(data)[1].previousSibling.children[3].innerText;
-                 d.properties.dateofinscription = d.properties.dateofinscription.replace(/\n/g, '').replace('Date of Inscription:', '');
-                 
-                 
+                //console.log($(data)[1]);
+                d.properties.country = $(data)[1].previousSibling.children[0].innerText;
+                d.properties.country = d.properties.country.replace(/\n/g, '');
+
+                d.properties.name = $(data)[1].previousSibling.children[1].innerText;
+                d.properties.name = d.properties.name.replace(/\n/g, '');
+
+                d.properties.dateofinscription = $(data)[1].previousSibling.children[3].innerText;
+                d.properties.dateofinscription = d.properties.dateofinscription.replace(/\n/g, '').replace('Date of Inscription:', '');
+
+
                 d.properties.extencion = $(data)[1].previousSibling.children[4].innerText;
-                 d.properties.extencion = d.properties.extencion.replace(/\n/g, '').replace('Extension:', '');
-                 
-                 
+                d.properties.extencion = d.properties.extencion.replace(/\n/g, '').replace('Extension:', '');
+
+
                 d.properties.criteria = $(data)[1].previousSibling.children[5].innerText;
-                 d.properties.criteria = d.properties.criteria.replace(/\n/g, '').replace('Criteria:', '');
-                 
-                 d.properties.property = $(data)[1].previousSibling.children[6].innerText;
-                 d.properties.property = d.properties.property.replace(/\n/g, '').replace('Property :', '');
-                 geojson.features.push(d);
+                d.properties.criteria = d.properties.criteria.replace(/\n/g, '').replace('Criteria:', '');
+
+                d.properties.property = $(data)[1].previousSibling.children[6].innerText;
+                d.properties.property = d.properties.property.replace(/\n/g, '').replace('Property :', '');
+
+
+
 
             }
 
 
+            ////////Cordinates
+            var cordinates = $(data)[1].previousSibling.children[2].innerText;
+            var cor = cordinates.split(" ");
+            //longitud
+            var days_lon = parseInt(cor[29].substring(1));
+            var minutes_lon = parseInt(cor[30]);
+            var seconds_lon = parseInt(cor[31]);
+            var direction_lon = cor[29].substring(0, 1);
+            //console.log(cor[29]);
+            var lon = get_lat_lon(days_lon, minutes_lon, seconds_lon, direction_lon);
+            //latitud
+            var days_lat = parseInt(cor[26].substring(1));
+            var minutes_lat = parseInt(cor[27]);
+            var seconds_lat = parseInt(cor[28]);
+            var direction_lat = cor[26].substring(0, 1);
+            //console.log(cor[26]);
+            var lat = get_lat_lon(days_lat, minutes_lat, seconds_lat, direction_lat);
+            //console.log('lat: ' + lat + '+ lon:' + lon);
+            d.geometry.coordinates[0] = lon;
+            d.geometry.coordinates[1] = lat;
+
+            geojson.features.push(d);
+
+
+
         }
 
-    });
+    }
+    );
 }
 ;
-
-
-
-
 window.setTimeout(function() {
     $('.content').append(all);
     $('.relatedContent').addClass('well');
@@ -110,7 +133,18 @@ window.setTimeout(function() {
 }, 3000);
 
 
+function get_lat_lon(days, minutes, seconds, direction) {
+    direction.toUpperCase();
+    var dd = days + minutes / 60 + seconds / (60 * 60);
+    //alert(dd);
+    if (direction == "S" || direction == "W")
+    {
+        dd = dd * -1;
+    } // Don't do anything for N or E return dd; }
 
+    return dd;
+}
+;
 
 
 
